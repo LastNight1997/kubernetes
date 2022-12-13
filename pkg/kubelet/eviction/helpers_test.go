ac732fc7dca50213acde63c36fcf76410c1fa682
vinay kulkarni <vskibum@gmail.com>
2022-12-13 09:54:43 +0000

    Fix unit test build break from rebase

--- pkg/kubelet/eviction/helpers_test.go
+++ pkg/kubelet/eviction/helpers_test.go
@@ -31,8 +31,11 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	featuregatetesting "k8s.io/component-base/featuregate/testing"
 	statsapi "k8s.io/kubelet/pkg/apis/stats/v1alpha1"
 
+	"k8s.io/kubernetes/pkg/features"
 	evictionapi "k8s.io/kubernetes/pkg/kubelet/eviction/api"
 	kubetypes "k8s.io/kubernetes/pkg/kubelet/types"
 )
